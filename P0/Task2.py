24704757893a329308fa019b6ef9435d3de0a377
viktor <viktordojnov@hotmail.com>
2020-05-17 22:04:06 -0700

    Complete Task2

--- P0/Task2.py
+++ P0/Task2.py
@@ -19,4 +19,30 @@ Print a message:
 "<telephone number> spent the longest time, <total time> seconds, on the phone during 
 September 2016.".
 """
+unique_nums = []
+time_on_phone = {}
+
+for call in calls:
+    if call[0] not in unique_nums:
+        unique_nums.append(call[0])
+        time_on_phone[call[0]] = int(call[3])
+    else:
+        time_on_phone[call[0]] += int(call[3])
+
+    if call[1] not in unique_nums:
+        unique_nums.append(call[1])
+        time_on_phone[call[1]] = int(call[3])
+    else:
+        time_on_phone[call[1]] += int(call[3])
+
+max_time = 0
+telephone = ''
+for number in time_on_phone:
+    if max_time < int(time_on_phone[number]):
+        max_time = time_on_phone[number]
+        telephone = number
+
+phrase = "{} spent the longest time, {} seconds, on the phone during September 2016.".format(telephone, max_time)
+print(phrase)
+
 
