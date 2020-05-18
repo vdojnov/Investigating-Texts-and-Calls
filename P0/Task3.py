89dca9df96e4d500cc4af29680c012eb937ccc9e
viktor <viktordojnov@hotmail.com>
2020-05-18 13:30:54 -0700

    Finish Task3

--- P0/Task3.py
+++ P0/Task3.py
@@ -32,7 +32,37 @@ Print the answer as part of a message:
 "The numbers called by people in Bangalore have codes:"
  <list of codes>
 The list of codes should be print out one per line in lexicographic order with no duplicates.
+"""
+prefixes = []
+telemarketer = False
+call_bangalore = 0
+total_calls = 0
+for call in calls:
+  code = ''
+  if call[0][0:5] == '(080)':
+    total_calls += 1
+    if call[1][0:5] == '(080)':
+      call_bangalore += 1
+    if call[1][0] == '(':
+      split = call[1].split(')')
+      code = split[0] + ')'
+    if call[1][0] == '7' or call[1][0] == '8' or call[1][0] == '9':
+      code = call[1][0:4]      
+    if call[1][0:3] == '140':
+      telemarketer = True
+    if code not in prefixes:
+      prefixes.append(code)
 
+if telemarketer:
+  prefixes.append('140')
+
+prefixes.sort()
+print("The numbers called by people in Bangalore have codes:")
+for prefix in prefixes:
+  print(prefix)
+
+
+"""
 Part B: What percentage of calls from fixed lines in Bangalore are made
 to fixed lines also in Bangalore? In other words, of all the calls made
 from a number starting with "(080)", what percentage of these calls
@@ -43,3 +73,6 @@ Print the answer as a part of a message::
 to other fixed lines in Bangalore."
 The percentage should have 2 decimal digits
 """
+
+percentage = (call_bangalore/total_calls) * 100
+print("{} percent of calls from fixed lines in Bangalore are calls to other fixed lines in Bangalore.".format(round(percentage, 2)))
